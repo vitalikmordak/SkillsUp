50dfa8fb453a421c2b894c443fc4a6b75560fdc3
vitalikmordak <vitalikmordak@gmail.com>
2018-12-07 20:01:30 +0200

    use 5 task with html form

---
+++ homework01/js/5.1.js
@@ -0,0 +1,34 @@
+function input() {
+    // var inputSize = document.getElementsByName("internationalSize").value;
+    var inputSize = document.getElementsByName("internationalSize").item(0).value;
+    switch (inputSize.toLocaleLowerCase().trim()) {
+        case 'xs':
+            printEuroSize("32-36");
+            break;
+        case 's':
+            printEuroSize("36-40");
+            break;
+        case 'm':
+            printEuroSize("40-44");
+            break;
+        case 'l':
+            printEuroSize("44-48");
+            break;
+        case 'xl':
+            printEuroSize("48-52");
+            break;
+        case 'xxl':
+            printEuroSize("52-56");
+            break;
+        default:
+            // set error message
+            document.getElementById("error").innerText = "[" + inputSize + "] is incorrect international size!";
+            return false;
+    }
+}
+
+
+function printEuroSize(size) {
+    document.write("European size is: " + size);
+    return true;
+}
